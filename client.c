263313c1c05eaacb6a547bca4d5f8b5443c2bdbe
bretttjohnson1 <swimbrettjohnson@gmail.com>
2016-09-20 23:25:41 -0400

    Initial Commit

---
+++ client.c
@@ -0,0 +1,62 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+void fail(){
+   printf("err %s \n",strerror(errno));
+   exit(1);
+}
+
+int main(int argc, char **argv){
+   if(argc !=2){
+      perror("Usage ./client <server_address>");
+      exit(1);
+   }
+   char *address = argv[1];
+   /*struct sockaddr_in6 sa;
+   char ip6[INET_ADDRSTRLEN];
+   inet_ntop(AF_INET6,&(sa.sin6_addr),ip6, INET6_ADDRSTRLEN);
+   printf("%s\n", ip6);*/
+
+   int status;
+   struct addrinfo hints;
+   struct addrinfo *serverinfo;
+   memset(&hints,0,sizeof(hints));
+   hints.ai_family = AF_UNSPEC;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_flags = AI_PASSIVE;
+   status = getaddrinfo(address, "8080", &hints,&serverinfo);
+
+   int sockfd = socket(serverinfo->ai_family,serverinfo->ai_socktype,serverinfo->ai_protocol);
+   //bind(sockfd, serverinfo->ai_addr,serverinfo->ai_addrlen);
+   int success = connect(sockfd,serverinfo->ai_addr,serverinfo->ai_addrlen);
+   if(success!=0){
+      fail();
+   }
+
+   long index = 0;
+   long size = 0;
+   success = recv(sockfd,&size,4,0);
+   if(success==-1){
+      fail();
+   }
+   //printf("%ld\n", size);
+   char buff[1];
+   while (index<size) {
+      success = recv(sockfd,buff,1,0);
+      if(success==-1){
+         fail();
+      }
+      fwrite(buff,1,1,stdout);
+      fflush(stdout);
+      index++;
+   }
+
+}
---
+++ server.c
@@ -0,0 +1,104 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+void fail(char *err){
+   printf("%s Error %s \n",err,strerror(errno));
+   exit(1);
+}
+int main(int argc, char **argv){
+
+
+   if(argc !=2){
+      perror("Usage ./server <filename>");
+      exit(1);
+   }
+   char *filename = argv[1];
+   int status;
+   struct addrinfo hints;
+   struct addrinfo *serverinfo;
+   memset(&hints,0,sizeof(hints));
+   hints.ai_family = AF_UNSPEC;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_flags = AI_PASSIVE;
+   status = getaddrinfo(NULL, "8080", &hints,&serverinfo);
+   if(status!=0){
+      printf("err %s \n",strerror(errno));
+      exit(1);
+   }
+   int sockfd = socket(serverinfo->ai_family,serverinfo->ai_socktype,serverinfo->ai_protocol);
+
+   int yes=1;
+   if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
+       perror("setsockopt");
+       exit(1);
+   }
+   bind(sockfd, serverinfo->ai_addr,serverinfo->ai_addrlen);
+   listen(sockfd, 5);
+   int newfd;
+   struct sockaddr_storage their_addr;
+   socklen_t addr_size;
+   addr_size = sizeof(their_addr);
+
+   FILE *f = fopen(filename,"rb");
+   char *filebuffer;
+   long filelength = 0;
+   fseek(f,0,SEEK_END);
+   filelength = ftell(f);
+
+   rewind(f);
+   filebuffer = (char *)malloc((filelength+1)*sizeof(char));
+   long var=0;
+   for(var =0;var<filelength;var++){
+         *(filebuffer+var)=getc(f);
+   }
+   *(filebuffer+filelength)='\0';
+   fclose(f);
+
+   printf("%ld\n",filelength );
+   fflush(stdout);
+   int success;
+   while(1){
+      printf("waiting...\n");
+      fflush(stdout);
+      while(1){
+         newfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+         if(newfd == -1)
+            close(newfd);
+         else{
+
+            success = send(newfd, &filelength,4,0);
+            if(success == -1){
+               fail("send length");
+            }
+            break;
+         }
+
+      }
+      long index = 0;
+
+      while(index<filelength){
+         success = send(newfd, &filebuffer[index],1,0);
+         if(success == -1){
+            printf("Transmission closed at %ld\n bytes",index );
+            //fail("send data");
+            break;
+         }
+         index+=1;
+      }
+      printf("sent data\n");
+      fflush(stdout);
+   }
+
+   free(filebuffer);
+   close(newfd);
+   close(sockfd);
+   exit(0);
+}
